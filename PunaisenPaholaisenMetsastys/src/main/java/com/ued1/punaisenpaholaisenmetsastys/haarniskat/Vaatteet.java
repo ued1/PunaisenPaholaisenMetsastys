554eb7ce4ab7387d2e63fce9b1565dcfc996b2a6
ued1 <ued1@users.noreply.github.com>
2015-05-16 19:11:36 +0300

    Haarniskakauppa

---
+++ PunaisenPaholaisenMetsastys/src/main/java/com/ued1/punaisenpaholaisenmetsastys/haarniskat/Vaatteet.java
@@ -0,0 +1,21 @@
+
+package com.ued1.punaisenpaholaisenmetsastys.haarniskat;
+
+public class Vaatteet implements Haarniska {
+
+    @Override
+    public String nimi() {
+        return "Vaatteet";
+    }
+
+    @Override
+    public int suojaa() {
+        return 2;
+    }
+
+    @Override
+    public int arvo() {
+        return 2;
+    }
+    
+}
--- PunaisenPaholaisenMetsastys/src/main/java/com/ued1/punaisenpaholaisenmetsastys/logiikka/HaarniskaKauppa.java
+++ PunaisenPaholaisenMetsastys/src/main/java/com/ued1/punaisenpaholaisenmetsastys/logiikka/HaarniskaKauppa.java
@@ -1,8 +1,48 @@
 
 package com.ued1.punaisenpaholaisenmetsastys.logiikka;
 
-public class HaarniskaKauppa {
+import com.ued1.punaisenpaholaisenmetsastys.haarniskat.*;
+import com.ued1.punaisenpaholaisenmetsastys.hahmot.Pelaaja;
+import java.util.ArrayList;
 
-    // paikka haarniskoiden ostoon ja myyntiin
+public class HaarniskaKauppa {
+    
+    private ArrayList<Haarniska> haarniskat;
+    
+    public HaarniskaKauppa() {
+        haarniskat = new ArrayList<>();
+        lisaaHaarniskatValikoimaan();
+        // lajittele haarniskat arvojärjestykseen
+    }
+    
+    private void lisaaHaarniskatValikoimaan() {
+        haarniskat.add(new Riepu());
+        haarniskat.add(new Vaatteet());
+    }
+    
+    public boolean voikoOstaaHaarniskat(Pelaaja pelaaja, Haarniska haarniska) {
+        if(pelaaja.getRahat() < haarniska.arvo()) {
+            return false;
+        }
+        return true;
+    }
+    
+    // aseista poiketen vanhaa haarniskaa ei myydä eikä siitä saa rahaa takaisin
+    public boolean ostaHaarniska(Pelaaja pelaaja, int haarniskanNumero) {
+        if(voikoOstaaHaarniskat(pelaaja, haarniskat.get(haarniskanNumero))) {
+            pelaaja.setHaarniska(haarniskat.get(haarniskanNumero));
+            pelaaja.muutaRahoja(0-haarniskat.get(haarniskanNumero).arvo());
+            return true;
+        }
+        return false;
+    }
+        
+    public String hinnastoMerkkijonona() {
+        String hinnasto = "";
+        for (int i = 0; i < haarniskat.size(); i++) {
+            hinnasto += "" + i + " " + haarniskat.get(i).nimi() + "  \t" + haarniskat.get(i).arvo() + "\n";
+        }
+        return hinnasto;
+    }
     
 }
